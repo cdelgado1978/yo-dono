7b831fd9a1d5a202b8fb56d9d0b8f7ad4f8a3f4c
Cesar Delgado <cesardelgado73@hotmail.com>
2020-08-20 21:43:28 -0400

    Inicial

---
+++ YoDono.do/YoDono.do/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YoDono.Models
+{
+    public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+}
